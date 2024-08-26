648797e9747f9a19d428fede4aeabd83a05d236d
Jamkris <dltmdgus1412@gmail.com>
2024-08-27 02:56:01 +0900

    ADD :: Splash CSS 연결

---
+++ src/pages/Splash/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as _ from './style';
+
+const Splash = () => {
+  return <_.Splash_Container></_.Splash_Container>;
+};
+
+export default Splash;
---
+++ src/pages/Splash/style.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Splash_Container = styled.div`
+
+`
