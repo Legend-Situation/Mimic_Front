30fa7e3f114e61b617aca2312a5ff40ab550814e
추성우 <cntjddn715@gmail.com>
2024-09-02 08:11:33 +0900

    chore :: 프로필 이미지 적용

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import * as _ from './style';
-import Profile from 'assets/image/Profile.png';
 
 interface ChatProps {
   image: string;
@@ -12,7 +11,7 @@ interface ChatProps {
 const Chat = ({ image, name, content, time }: ChatProps) => {
   return (
     <_.Chat_Layout>
-      <_.Chat_Image src={Profile} />
+      <_.Chat_Image src={image} />
       <_.Chat_Detail>
         <_.Chat_Name>{name}</_.Chat_Name>
         <_.Chat_Content>{content}</_.Chat_Content>
